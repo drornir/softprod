546c36d00ea12d7ecd2473f13c3ddab2af1d1059
Dror <dexenerator@gmail.com>
2016-11-16 23:47:38 +0200

    dict comprehension

--- main.py
+++ main.py
@@ -44,18 +44,18 @@ def get_k_edge(k, V, E):
     assert V == range(len(V))
     colors = range(k)
     # variables = [[Bool('v_{}_color_{}'.format(v, c)) for c in colors] for v in V]
-    edgesColors = [[Bool('e_{}_col_{}'.format(e, c)) for c in colors] for e in E]
+    edges_colors = {e: [Bool('e_{}_col_{}'.format(e, c)) for c in colors] for e in E}
 
     s = Solver()
     # at least one color per edge
     for e in E:
-        s.add(Or([edgesColors[e][c] for c in colors]))
+        s.add(Or([edges_colors[e][c] for c in colors]))
 
     # at most one ccolor p edge
     for e in E:
         for c1 in range(k):
             for c2 in range(c1 + 1, k):
-                s.add(Or(Not(edgesColors[e][c1]), Not(edgesColors[e][c2])))
+                s.add(Or(Not(edges_colors[e][c1]), Not(edges_colors[e][c2])))
 
     #
     for e1 in E:
@@ -63,7 +63,7 @@ def get_k_edge(k, V, E):
             if e1 != e2:
                 if e1[0] == e2[0] or e1[1] == e2[0] or e1[0] == e2[1] or e1[1] == e2[1]:
                     for c in colors:
-                        s.add(Or(Not(edgesColors[e1][c]), Not(edgesColors[e2][c])))
+                        s.add(Or(Not(edges_colors[e1][c]), Not(edges_colors[e2][c])))
 
 
 def draw_graph(V, E, coloring={}, filename='graph', engine='circo', directed=False):
@@ -92,6 +92,7 @@ def draw_graph(V, E, coloring={}, filename='graph', engine='circo', directed=Fal
 
 
 if __name__ == '__main__':
+    get_k_edge(5,Petersen_V,Petersen_E)
     #
     # Your tests here...
     #
