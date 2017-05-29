e4e31d499b742bc38949378436f82473e125e773
guynaa <guynaa@gmail.com>
2017-05-29 20:33:04 +0300

    changed inLine implementation

--- ivy/AxiomGenerator.java
+++ ivy/AxiomGenerator.java
@@ -6,9 +6,40 @@ import java.util.List;
 public class AxiomGenerator {
 
     public static void main(String[] args) {
+
+//        axioms();
 	// write your code here
 
+        conjectures();
 
+
+
+
+    }
+
+    private static void conjectures() {
+
+        List<String> vars = new ArrayList<>();
+        vars.add("inLinel0");
+        for (int i = 0; i < 8; i++) {
+            vars.add("inLinee" + i);
+        }
+
+        for (int i = 0; i < 6; i++) {
+            vars.add("inLined" + i);
+        }
+
+        vars.add("inLinefinishedLine");
+        vars.add("inLinedReturn");
+
+        System.out.println("#automatically generated conjectures");
+        for (int i = 0; i < vars .size(); i++)
+            for (int j = i + 1; j < vars .size(); j++)
+                //System.out.println("axiom "+ vars.get(i) + "~=" + vars.get(j));
+                System.out.println("conjecture " + vars.get(i)+"(T1) & " + vars.get(j) +"(T2) -> T1 ~= T2");
+    }
+
+    private static void axioms() {
         List<String> vars = new ArrayList<>();
         vars.add("l0");
         for (int i = 0; i < 8; i++) {
